f3d39e6b1c28cfae55edd90c0147d8d86434f36c
Jakub <jakub7zalewski7@gmail.com>
2023-05-17 14:01:26 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+// import { useState } from "react"
+// import reactLogo from "./assets/react.svg"
+import Header from "./components/Header"
+// import viteLogo from "/vite.svg"
+// import axios from 'axios';
+
+function App() {
+	return (
+		<>
+			<Header />
+		</>
+	)
+}
+
+export default App
